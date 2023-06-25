f0f6b5cc42f62e09fefdd8e89c31618346f67554
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-06-25 09:04:06 +0300

    adds changes

--- tweet_card/vite.config.js
+++ tweet_card/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["web"],
+      polyfills: ["es.promise", "es.symbol"],
+      fallback: {
+        entry: "/tweet_card/main.js",
+        html: "/tweet_card/index.html",
+        dist: "dist",
+      },
+    }),
+  ],
   base: "/tweet_card/",
   resolve: {
     alias: {
@@ -11,3 +22,17 @@ export default defineConfig({
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/tweet_card/",
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//     },
+//   },
+// });
